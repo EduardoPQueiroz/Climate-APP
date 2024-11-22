abdacd8028214066a9a2752601f70798db728173
Eduardo Pereira de Queiroz <eduardopq4@gmail.com>
2024-11-22 18:41:19 -0300

    starting project: creating classes, services and viewmodels

---
+++ ClimateApp/ClimateApp/Services/WeatherResponseServices.cs
@@ -0,0 +1,92 @@
+﻿using ClimateApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ClimateApp.Services
+{
+    internal class WeatherResponseServices
+    {
+        private HttpClient httpClient;
+        private ObservableCollection<WeatherResponse> weatherResponses; //isso é para um get geral, que traz várias, ou seja uma collection
+        private WeatherResponse weatherResponse; //isso é para get específicos, como getbyid por exemplo
+        private JsonSerializerOptions jsonSerializerOptions; // configurar/formatar o JSON
+        Uri uri = new Uri("http://api.openweathermap.org/data/2.5/weather?q=");
+        String key = "uk&APPID=d1d7451a2cdd07198bbf237dde66f703";
+
+        public WeatherResponseServices()
+        {
+            httpClient = new HttpClient();
+            jsonSerializerOptions = new JsonSerializerOptions
+            {
+                //propriedades dos serializer options
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true,
+            };
+        }
+
+        public async Task<ObservableCollection<WeatherResponse>> GetInformacoesAsync() // TASK: usado no await
+        {
+
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync(uri);//quero saber todos os posts;
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();// tranforma o conteudo em string;
+                    weatherResponses = JsonSerializer.Deserialize<ObservableCollection<WeatherResponse>>(content, jsonSerializerOptions);
+                }
+            }
+            catch
+            {
+
+            }
+            return weatherResponses;
+        }
+
+        public async Task<WeatherResponse> GetWeatherByCidadeAsync(String cidade) // TASK: usado no await
+        {
+            Debug.WriteLine("Chamou!! o GetSalaByIdAsync");
+            WeatherResponse weatherResponse = new WeatherResponse
+                ();
+            try
+            {
+                HttpResponseMessage response = await httpClient.GetAsync($"{uri}{cidade}{key}");//quero saber todos os posts;
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();// tranforma o conteudo em string;
+
+                    Debug.WriteLine($"Resposta JSON: {content}");
+
+                    weatherResponse = JsonSerializer.Deserialize<WeatherResponse>(content, jsonSerializerOptions);
+                }
+                else
+                {
+                    // se der erro na chama da API mostra
+                    Debug.WriteLine($"Erro na chamada à API: {response.StatusCode}");
+                }
+            }
+            catch (JsonException ex)
+            {
+                // se der alguma exeption ai mostra
+                Debug.WriteLine($"Exceção ocorrida: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                // se der alguma exeption ai mostra
+                Debug.WriteLine($"Exceção ocorrida: {ex.Message}");
+            }
+
+            return weatherResponse;
+        }
+
+    }
+
+
+}
---
+++ ClimateApp/ClimateApp/ViewModels/WeatherResponseViewModel.cs
@@ -0,0 +1,56 @@
+﻿using ClimateApp.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClimateApp.ViewModels
+{
+    internal partial class WeatherResponseViewModel : ObservableObject, INotifyPropertyChanged
+    {
+        [ObservableProperty]
+        public Coordinates coord; 
+
+        [ObservableProperty]
+        public List<Weather> weather;
+
+        [ObservableProperty]
+        public string Base; 
+
+        [ObservableProperty]
+        public Main main;
+
+        [ObservableProperty]
+        public int visibility;
+
+        [ObservableProperty]
+        public Wind wind;
+
+        [ObservableProperty]
+        public Clouds clouds;
+
+        [ObservableProperty]
+        public long dt;
+
+        [ObservableProperty]
+        public Sys sys; 
+
+        [ObservableProperty]
+        public int timezone;
+
+        [ObservableProperty]
+        public int id;
+
+        [ObservableProperty]
+        public string name;
+
+        [ObservableProperty]
+        public int cod;
+
+
+        WeatherResponseServices weatherResponseServices = new WeatherResponseServices();
+    }
+}
